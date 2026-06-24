7c52a30c06210e76d5224590f59a7ca9bbbd26b9
navaraj <tamangnavaraj507@gmail.com>
2026-06-24 10:14:12 +0545

    Add script to calculate and display average marks of students
    - Created a new file lab2.js
    - Implemented functionality to store and display marks of 5 students
    - Used forEach loop to iterate through marks and calculate the sum
    - Calculated and displayed the average marks

---
+++ lab2.js
@@ -0,0 +1,17 @@
+// Store marks of 5 students
+let marks = [70, 85, 90, 60, 75];
+
+let sum = 0;
+
+// Display marks using forEach loop
+console.log("Marks of 5 students:");
+marks.forEach(function(mark, index) {
+    console.log("Student " + (index + 1) + ": " + mark);
+    sum += mark;
+});
+
+// Calculate average
+let average = sum / marks.length;
+
+// Display average
+console.log("Average Marks: " + average);
